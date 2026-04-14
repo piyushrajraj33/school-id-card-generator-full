c9b8d63179642fc270aba994f4a07d58e9a4e685
piyushrajraj33 <piyushrajraj33@example.com>
2026-04-14 18:40:02 +0530

    ec2 deploy

--- server.js
+++ server.js
@@ -38,7 +38,16 @@ const basicAuth = (req, res, next) => {
 
 app.use('/admin', basicAuth, adminRoutes)
 
-const PORT = 3000
-app.listen(PORT,()=>{
-console.log("Server running on http://localhost:"+PORT)
-})
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Running on EC2 with PM2!');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// app.listen(PORT,()=>{
+// console.log("Server running on http://localhost:"+PORT)
+// })
